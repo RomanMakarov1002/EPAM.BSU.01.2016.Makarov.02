76645399d73bf9289bc1af563c3ec1d1bb49d60f
RomanMakarov1002 <roman.makarov1002@gmail.com>
2016-03-14 13:53:02 +0300

    Task 4 (Customer class + string formatter) + NUnit tests

---
+++ Task4Day2/NunitTests/CustomersClassTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Task4;
+using  static Task4.Customer;
+
+
+namespace NunitTests
+{
+    [TestFixture]
+    public class CustomersClassTests
+    {
+        [Test]
+        public void CustomerToString_Test()
+        {
+            Customer customer = new Customer("Jeffrey Richter", "+1 (425) 555-0100", 1000000);
+
+            string[] testFormats = new string[3] {"123","12","13"};
+            string[] expectedResults = new string[3];
+            decimal revenue = 1000000;
+            expectedResults[0]= "Customer record: Jeffrey Richter, " + revenue.ToString("N0") + ", +1 (425) 555-0100";
+            expectedResults[1] = "Customer record: Jeffrey Richter, " + revenue.ToString("N0");
+            expectedResults[2] = "Customer record: Jeffrey Richter, +1 (425) 555-0100";
+            for (int i = 0; i < testFormats.Length; i++)
+                Assert.AreEqual(expectedResults[i], customer.ToString(testFormats[i]));
+        }
+
+        [Test]
+        public void NameFormatter_Test()
+        {
+            Customer customer = new Customer("Jeffrey Richter", "+1 (425) 555-0100", 1000000);
+            string expecterResult = "Customer record: Jeffrey, " +
+                                    customer.Revenue.ToString("N0") + ", +1 (425) 555-0100";
+            Assert.AreEqual(expecterResult,customer.NameFormatter(7));
+        }
+
+        [Test]
+        public void PhoneFormatter_Test()
+        {
+            Customer customer = new Customer("Jeffrey Richter", "+1 (425) 555-0100", 1000000);
+            string expecterResult = "Customer record: Jeffrey Richter, " +
+                                    customer.Revenue.ToString("N0") + ", 555-0100";
+            Assert.AreEqual(expecterResult, customer.PhoneFormatter());
+        }
+    }
+    
+}
---
+++ Task4Day2/Task4/Customer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public class Customer
+    {
+        public string Name { get; }
+        public string ContactPhone { get; }
+        public decimal Revenue { get; }
+
+        public Customer(string name, string phone, decimal revenue)
+        {
+            this.Name = name;
+            this.ContactPhone = phone;
+            this.Revenue = revenue;
+        }
+
+        public override string ToString()
+        {
+            return ToString("123");
+        }
+
+        public string ToString(string format)
+        {
+
+        switch (format)
+            {
+                case "123":
+                    return string.Format("Customer record: {0}, {1}, {2}", Name, Revenue.ToString("N0"), ContactPhone);
+                case "12":
+                    return string.Format("Customer record: {0}, {1}", Name, Revenue.ToString("N0"));
+                case "13":
+                    return string.Format("Customer record: {0}, {1}", Name, ContactPhone);
+                case "23":
+                    return string.Format("Customer record: {0}, {1}", Revenue.ToString("N0"), ContactPhone);
+                case "1":
+                    return string.Format("Customer record: {0}", Name);
+                case "2":
+                    return string.Format("Customer record: {0}", Revenue.ToString("N0"));
+                case "3":
+                    return string.Format("Customer record: {0}", ContactPhone);               
+
+                default:
+                    throw new ArgumentException(string.Format("'{0}' is an invalid format string", format));
+            }
+        }
+    }
+
+    public static class OtherClass
+    {
+        public static string NameFormatter(this Customer customer, int length)
+        {
+            return string.Format("Customer record: {0}, {1}, {2}", customer.Name.Substring(0,length), customer.Revenue.ToString("N0"), customer.ContactPhone );           
+        }
+
+        public static string PhoneFormatter(this Customer customer)
+        {
+            return string.Format("Customer record: {0}, {1}, {2}", customer.Name, customer.Revenue.ToString("N0"), customer.ContactPhone.Substring(9));
+        }
+    }
+}
