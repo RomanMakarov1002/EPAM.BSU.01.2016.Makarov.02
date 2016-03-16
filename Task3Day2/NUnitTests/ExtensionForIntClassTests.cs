b49f5c51dd8469f209dd4f061a861a8dd4e9b71f
RomanMakarov1002 <roman.makarov1002@gmail.com>
2016-03-16 19:55:19 +0300

    Update

--- Task3Day2/NUnitTests/ExtensionForIntClassTests.cs
+++ Task3Day2/NUnitTests/ExtensionForIntClassTests.cs
@@ -1,23 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
-using static ExtensionForInt.Extensions;
+using static ExtensionForInt.HexFormatProvider;
+using System.Threading;
+using ExtensionForInt;
 
 namespace NUnitTests
 {
     [TestFixture]
     public class ExtensionForIntClassTests
     {
-        [Test]
-        public void IntFormatToHex_Test()
+
+        [TestCase(145, Result = "0x91")]
+        [TestCase(-145, Result = "-0x91")]
+        [TestCase(0, Result = "0x0")]
+        [TestCase(41837, Result = "0xA36D")]
+        [TestCase(47, Result = "0x2F")]
+        [TestCase(47.2, ExpectedException = typeof(ArgumentException))]
+        public string Format_Test(object number)
         {
-            int[] testNums = new int[10] { 32767, 6400, 152, 10, 1, 0, -1, -152, -32767, 16 };
-            string[] expectedResults = new string[10] {"7FFF", "1900", "98", "A", "1", "0", "FFFFFFFF", "FFFFFF68", "FFFF8001", "10"};
-            for (int i=0; i<testNums.Length; i++)
-                Assert.AreEqual(expectedResults[i], testNums[i].IntFormatToHex());
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;            
+           IFormatProvider fp = new HexFormatProvider();
+            return string.Format(fp, "{0:H}", number);
         }
+
+
+        [TestCase(47, "{0:X}", Result = "2F")]
+        [TestCase(.473, "{0:P}", Result = "47.30 %")]
+        [TestCase(.473, "{0:P0}", Result = "47 %")]
+        [TestCase(4.73, "{0:C}", Result = "¤4.73")]
+        [TestCase(4.73, "{0:C}", Result = "¤4.73")]
+        [TestCase(4.7321, "{0:F2}", Result = "4.73")]
+        public string ParentFormat_Test(object number, string format)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            IFormatProvider fp = new HexFormatProvider();
+            return string.Format(fp, format, number);
+        }
+
+
+        
     }
 }
